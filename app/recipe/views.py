d34dcb3dcd2e51a94fa68655341725574640fbac
SaurabhG02 <saurabhghevade02@gmail.com>
2023-10-11 00:05:41 +0530

    tag functioanlity

---
+++ app/recipe/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render
+from rest_framework import viewsets, mixins
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+from core.models import Recipe, Tag
+from recipe import serializers
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    """View for manage recipe APIs."""
+    serializer_class = serializers.RecipeDetailSerializer
+    queryset = Recipe.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """
+        Retrieve the recipes for the authenticated user.
+        
+        Filter the queryset based on the user and order it by descending id.
+        """
+        user = self.request.user
+        return self.queryset.filter(user=user).order_by('-id')
+    
+    def get_serializer_class(self):
+        """Return appropriate serializer class."""
+        if self.action == 'list':
+            return serializers.RecipeSerializer
+        
+        return self.serializer_class
+        
+    def perform_create(self, serializer):
+        """Create a new recipe."""
+        serializer.save(user=self.request.user)
+
+
+class TagViewSet(mixins.UpdateModelMixin,
+                mixins.ListModelMixin,
+                mixins.DestroyModelMixin,
+                viewsets.GenericViewSet):
+    """View for manage tag APIs."""
+    serializer_class = serializers.TagSerializer
+    queryset = Tag.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Retrieve the tags for the authenticated user."""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+    
+
+
+        
